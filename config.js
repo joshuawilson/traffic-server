79ac8a2dc60a5f7021479625559f752593c41940
mwcz <mwc@clayto.com>
2016-04-25 13:01:45 -0400

    send spam and ws messages more frequently

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HTTP_PORT     : 3003,
     WS_PORT       : 3004,
-    WS_INTERVAL   : 10, // ms
-    SPAM_INTERVAL : 30, // ms
+    WS_INTERVAL   : 5, // ms
+    SPAM_INTERVAL : 10, // ms
 };
