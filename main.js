f9d9be4b88dfd82560ea0f34e5230ee3a3803517
mwcz <mwc@clayto.com>
2016-04-25 13:02:24 -0400

    don't crash on sending message to disconnected client

--- main.js
+++ main.js
@@ -54,7 +54,11 @@ wss.on('connection', function connection(ws) {
 
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
-        client.send(data);
+        try {
+            client.send(data);
+        } catch (e) {
+            console.error('tried to send websocket message to closed client; ignoring.');
+        }
     });
 };
 
